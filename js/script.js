c5c329628ffe276cea45fb5b386f04d3324f659e
pubgespyt <pubgespyt@gmail.com>
2020-06-10 23:22:43 -0700

    up

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function desiespdown(){
+  window.location.href = "https://gadgetsworld.website/IMZxAVF";
+}
+
+function pubgesp(){
+  window.location.href = "https://gadgetsworld.website/BLzY";
+}
+
+function pubgespyt(){
+  window.location.href = "https://www.youtube.com/channel/UCSNIPF64QbDrHICPBn3Gdmw/";
+}
